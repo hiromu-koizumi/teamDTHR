f0033e7c95abd3737439f40ad395f87790e04132
hiromu-koizumi <abn21029@gmail.com>
2018-11-21 15:08:09 +0900

    Initial Commit

---
+++ idolApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  idolApp
+//
+//  Created by 小泉大夢 on 2018/11/21.
+//  Copyright © 2018 小泉大夢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
